9dd20f0787dd0dc52187c958c25bbdb885dea483
daff002 <daff002@gmail.com>
2024-06-11 19:13:20 +0500

    ДЗ 1

--- urban/homework2.py
+++ urban/homework2.py
@@ -4,4 +4,4 @@ name_of_course = 'Python'
 time_for_task = count_hours / count_homework
 
 print(f'{name_of_course}, всего задач: {count_homework}, затрачено часов: {count_hours}, '
-      f'среднее время выполнения {time_for_task} часа')
+      f'среднее время выполнения {time_for_task} часа.')
