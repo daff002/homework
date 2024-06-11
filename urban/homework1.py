1107607630598514eb5970c864cf43f2dfb0dc90
daff002 <daff002@gmail.com>
2024-06-11 17:33:35 +0500

    init

---
+++ urban/homework1.py
@@ -0,0 +1,28 @@
+# 1
+example = 'Топинамбур'
+
+# 2
+print(example[0])
+
+# 3
+print(example[-1])
+
+# 4 тут все неоднозначно, если мы знаем значение переменной example, то решение такое
+print(example[5:])
+
+# если значение переменой всегда разное, то нужно определить четное или нечетное количество символов в строке
+
+if len(example) % 2 == 0:                # если остаток от деления 0 (четное)
+    start_index = len(example) // 2      # то делим на 2
+else:                                    # в других случаях
+    start_index = len(example) // 2 + 1  # делим на 2 и прибавляем 1
+
+second_half = example[start_index:]
+
+print(second_half)
+
+# 5
+print(example[::-1])
+
+# 6
+print(example[1::2])
---
+++ urban/homework2.py
@@ -0,0 +1,7 @@
+count_homework = 12
+count_hours = 1.5
+name_of_course = 'Python'
+time_for_task = count_hours / count_homework
+
+print(f'{name_of_course}, всего задач: {count_homework}, затрачено часов: {count_hours}, '
+      f'среднее время выполнения {time_for_task} часа.')
